0f7eb06326f88c258e222be24c0fb0842528137d
jaeilssss <jaeilssss@bitbucket.org>
2025-03-03 21:51:26 +0900

    init

---
+++ src/main/kotlin/com/market/studyboardkt/StudyBoardKtApplication.kt
@@ -0,0 +1,11 @@
+package com.market.studyboardkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyBoardKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyBoardKtApplication>(*args)
+}
